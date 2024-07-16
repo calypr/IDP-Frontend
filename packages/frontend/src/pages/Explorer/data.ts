0e910951c1f7d73431e67dc5c499cca9a3d0bd4d
matthewpeterkort <matthewpeterkort@gmail.com>
2024-07-16 15:31:51 -0700

    fixes

--- packages/frontend/src/pages/Explorer/data.ts
+++ packages/frontend/src/pages/Explorer/data.ts
@@ -1,25 +1,20 @@
 import { GetServerSideProps } from 'next';
 import { getNavPageLayoutPropsFromConfig } from '../../lib/common/staticProps';
+import ContentSource from '../../lib/content';
+import { CohortBuilderConfiguration } from '../../features/CohortBuilder';
+import { GEN3_COMMONS_NAME } from '@gen3/core';
 import type { NavPageLayoutProps } from '../../features/Navigation';
 
 export const ExplorerPageGetServerSideProps: GetServerSideProps<
   NavPageLayoutProps
 > = async (_context) => {
   try {
-    const response = await fetch(
-      'http://10.43.124.240:9200/explorer_config/_doc/1',
-    );
-
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const explorerConfig = await response.json();
-
+    const cohortBuilderProps: CohortBuilderConfiguration =
+      await ContentSource.get(`config/${GEN3_COMMONS_NAME}/explorer.json`);
     return {
       props: {
         ...(await getNavPageLayoutPropsFromConfig()),
-        explorerConfig: explorerConfig.results,
+        explorerConfig: cohortBuilderProps,
       },
     };
   } catch (err) {
