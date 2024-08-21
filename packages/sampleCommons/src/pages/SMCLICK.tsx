f8f20fc0a7af4df44923e8487cfcdd3deaa0f204
quinnwai <quinnwai.wong@gmail.com>
2024-08-21 00:38:25 -0700

    updated SMMART report page: counts and charts

--- packages/sampleCommons/src/pages/SMCLICK.tsx
+++ packages/sampleCommons/src/pages/SMCLICK.tsx
@@ -19,20 +19,16 @@ import {
   getNavPageLayoutPropsFromConfig,
 } from '@gen3/frontend';
 
-import { useGeneralGQLQuery } from '@gen3/core';
+import { fieldNameToTitle, useGeneralGQLQuery } from '@gen3/core';
 import { title } from 'process';
 
+////////////////
+// INTERFACES //
+////////////////
 interface SamplePageProps {
   headerProps: any;
   footerProps: any;
 }
-
-const isQueryResponse = (obj: any): obj is QueryResponse => {
-  return (
-    typeof obj === 'object' &&
-    (obj.data === undefined || typeof obj.data === 'object')
-  );
-};
 
 interface HistogramData {
   key: string;
@@ -50,6 +46,16 @@ interface QueryResponse {
   data?: Record<string, Observation[]>;
   countsProperty?: string;
 }
+
+/////////////
+// HELPERS //
+/////////////
+const isQueryResponse = (obj: any): obj is QueryResponse => {
+  return (
+    typeof obj === 'object' &&
+    (obj.data === undefined || typeof obj.data === 'object')
+  );
+};
 
 const extractData = (
   data: QueryResponse,
@@ -83,12 +89,11 @@ const extractData = (
   return 0;
 };
 
-const HorizontalBarChart = ({ headerProps, footerProps }: SamplePageProps) => {
-  const summaryCountsQuery = (countsProperty: string) => {
+const summaryCountsQuery = (resourceType: string, countsProperty: string) => {
     const summary_counts_query = {
       query: `query ($filter: JSON){
       _aggregation {
-        observation(filter: $filter, accessibility: all) {
+        ${resourceType}(filter: $filter, accessibility: all) {
           ${countsProperty} {
             _cardinalityCount
             }
@@ -110,108 +115,159 @@ const HorizontalBarChart = ({ headerProps, footerProps }: SamplePageProps) => {
     return summary_counts_query;
   };
 
-  const countsQuery = (countsProperty: string) => {
-    const props_query = {
-      query: `query ($filter: JSON) {
-        _aggregation{
-          observation(filter: $filter accessibility:all) {
-            ${countsProperty}{
-              histogram {
-                key
-                count
-              }
+const countsQuery = (countsProperty: string) => {
+  const props_query = {
+    query: `query ($filter: JSON) {
+      _aggregation{
+        observation(filter: $filter accessibility:all) {
+          ${countsProperty}{
+            histogram {
+              key
+              count
             }
           }
         }
-      }`,
-      variables: {
-        filter: {
-          AND: [
-            {
-              IN: {
-                project_id: ['cbds-smmart_labkey_demo'],
-              },
+      }
+    }`,
+    variables: {
+      filter: {
+        AND: [
+          {
+            IN: {
+              project_id: ['cbds-smmart_labkey_demo'],
             },
-          ],
-        },
+          },
+        ],
       },
-    };
-    return props_query;
+    },
   };
+  return props_query;
+};
 
-  const SummaryCounts = (countsProperty: string) => {
-    const { data, isLoading, isError } = useGeneralGQLQuery(
-      summaryCountsQuery(countsProperty),
-    );
-    const totalCountsData = isQueryResponse(data)
-      ? extractData(data, countsProperty, 'totalCounts')
-      : 0;
-    if (isError) {
-      return <ErrorCard message={'Error occurred while fetching data'} />;
-    }
-    return isLoading ? (
-      <div>loading...</div>
-    ) : (
-      <div className="text-2xl font-bold">
-        {typeof totalCountsData === 'number' ? (
-          totalCountsData
-        ) : (
-          <>{'missing data'}</>
-        )}
-      </div>
-    );
-  };
+const countsFromField = (resourceType: string, countsProperty: string) => {
+  const { data, isLoading, isError } = useGeneralGQLQuery(
+    summaryCountsQuery(resourceType, countsProperty),
+  );
+  const totalCountsData = isQueryResponse(data)
+    ? extractData(data, countsProperty, 'totalCounts')
+    : 0;
+  if (isError) {
+    return <ErrorCard message={'Error occurred while fetching data'} />;
+  }
+  return isLoading ? (
+    <div>loading...</div>
+  ) : (
+    <div className="text-2xl font-bold">
+      {typeof totalCountsData === 'number' ? (
+        totalCountsData
+      ) : (
+        <>{'missing data'}</>
+      )}
+    </div>
+  );
+};
 
-  const Echart = (countsProperty: string) => {
-    const { data, isLoading, isError } = useGeneralGQLQuery(
-      countsQuery(countsProperty),
-    );
-    const queryData = isQueryResponse(data)
-      ? extractData(data, countsProperty, 'histogram')
-      : [];
-    if (isError) {
-      return <ErrorCard message={'Error occurred while fetching data'} />;
-    }
-    const option = {
-      title: {
-        text: countsProperty,
-        right: 50,
+const chartFromField = (countsProperty: string) => {
+  const { data, isLoading, isError } = useGeneralGQLQuery(
+    countsQuery(countsProperty),
+  );
+  const queryData = isQueryResponse(data)
+    ? extractData(data, countsProperty, 'histogram')
+    : [];
+  if (isError) {
+    return <ErrorCard message={'Error occurred while fetching data'} />;
+  }
+  const option = {
+    title: {
+      text: fieldNameToTitle(countsProperty),
+      right: 50,
+    },
+    yAxis: {
+      type: 'category',
+      data: Array.isArray(queryData) ? queryData.map((item) => item.key) : [],
+      axisLabel: {
+        interval: 0,
       },
-      yAxis: {
-        type: 'category',
-        data: Array.isArray(queryData) ? queryData.map((item) => item.key) : [],
-        axisLabel: {
-          interval: 0,
-        },
+    },
+    xAxis: {
+      type: 'value',
+    },
+    grid: {
+      containLabel: true,
+    },
+    series: [
+      {
+        data: Array.isArray(queryData)
+          ? queryData.map((item) => item.count)
+          : [],
+        type: 'bar',
+        barCategoryGap: '50%',
       },
-      xAxis: {
-        type: 'value',
-      },
-      grid: {
-        containLabel: true,
-      },
-      series: [
-        {
-          data: Array.isArray(queryData)
-            ? queryData.map((item) => item.count)
-            : [],
-          type: 'bar',
-          barCategoryGap: '50%',
-        },
-      ],
-    };
-    const chart = (
-      <div className="w-full h-full">
-        <LoadingOverlay visible={isLoading} />
-        <ReactECharts
-          option={option}
-          style={{ height: '100%', minHeight: '500px' }}
-        />
-      </div>
-    );
-    return chart;
+    ],
   };
+  const chart = (
+    <div className="w-full h-full">
+      <LoadingOverlay visible={isLoading} />
+      <ReactECharts
+        option={option}
+        style={{ height: '100%', minHeight: '500px' }}
+      />
+    </div>
+  );
+  return chart;
+};
+
+const countsByResource = (resourceType: string) => {
+  const { data, isLoading, isError } = useGeneralGQLQuery(
+    countsByResourceQuery(resourceType),
+  );
+  const count = isQueryResponse(data)
+    ? data.data._aggregation.file._totalCount
+    : 0
+  return (
+    <div className="text-2xl font-bold">
+      {count}
+    </div>
+  )
+}
+
+const countsByResourceQuery = (resourceType: string) => {
+  return {
+    query: `query ($filter: JSON) {
+      _aggregation {
+        ${resourceType}(filter: $filter accessibility:all) {
+          _totalCount
+        }
+      }
+    }`,
+    variables: {
+      filter: {
+        AND: [
+          {
+            IN: {
+              project_id: ['cbds-smmart_labkey_demo'],
+            },
+          },
+        ],
+      },
+    },
+  };
+}
+
+
+
+///////////////
+// COMPONENT //
+///////////////
+const HorizontalBarChart = ({ headerProps, footerProps }: SamplePageProps) => {
   const router = useRouter();
+
+  // TODO: refactor out into a config
+  const chartFields = ['enrollment_diagnosis', 'protocol_library_type', 'biopsy_anatomical_location'];
+  const numChartCols = chartFields.length <= 3 ? chartFields.length : 3
+  const countsFields = ['patient_id', 'specimen_identifier', 'specimen_collection_concept', 'clinical_trials']
+  const countsTitles = ['Patients', 'Specimens', 'Cancers', 'Clinical Trials']
+
 
   return (
     <ProtectedContent>
@@ -232,7 +288,7 @@ const HorizontalBarChart = ({ headerProps, footerProps }: SamplePageProps) => {
             </div>
 
             <div className="grid grid-cols-3 gap-4 mt-10">
-              <div className="col-span-1 p-10">
+              <div className="p-10">
                 <h1 className="prose sm:prose-base 2xl:prose-lg mb-5 !mt-0">
                   Overview of SMMART and datasets, what can be found in this
                   project
@@ -246,36 +302,28 @@ const HorizontalBarChart = ({ headerProps, footerProps }: SamplePageProps) => {
                   Explore Datasets
                 </Button>
               </div>
-
-              <div className="col-span-2 grid grid-cols-2 gap-4">
-                {Echart('condition_code')}
-
-                {Echart('product_notes_project_id')}
+              <div className={`col-span-${numChartCols} grid grid-cols-${numChartCols} gap-2`}>
+                {chartFields.map(field => chartFromField(field))}
               </div>
             </div>
             <div className="text-center mx-auto bg-gray-200 py-5">
               <div className="flex justify-center space-x-8">
                 <div className="text-center">
-                  {SummaryCounts('patient_id')}
-                  <div className="text-sm">Donors</div>
+                  {countsFromField('observation', 'specimen_identifier')}
+                  <div className="text-sm">Specimens</div>
                 </div>
                 <div className="text-center">
-                  {SummaryCounts('specimen_identifier')}
-                  <div className="text-sm">Samples</div>
+                  {countsByResource('file')}
+                  <div className="text-sm">Files</div>
                 </div>
-                <div className="text-center">
-                  <div className="text-2xl font-bold">1</div>
-                  <div className="text-sm">Datasets</div>
-                </div>
-                <div className="text-center">
-                  {SummaryCounts('condition_code')}
-
-                  <div className="text-sm">Cancers</div>
-                </div>
-                <div className="text-center">
-                  {SummaryCounts('specimen_collection_concept')}
-                  <div className="text-sm">Collection Types</div>
-                </div>
+              {/* {countsFields.map((field, i) => {
+                return (
+                  <div className="text-center">
+                    {countsFromField(field)}
+                    <div className="text-sm">{countsTitles[i]}</div>
+                  </div>
+                )
+              })} */}
               </div>
             </div>
           </div>
