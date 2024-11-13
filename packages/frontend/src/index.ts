3d17b4e02b87311e0c42b30a177b5e6c656ddec1
Ali Lahrime <90918464+alilahrime@users.noreply.github.com>
2024-11-13 11:34:05 -0500

    PXP-11325: Implement Analysis Card (#191)
    * feat(PXP-11325): implement analysis cards
    
    * fix lint issue
    
    * add REPL example
    
    * add AnalyticsCard
    
    * add AnalyticsCard
    
    * add analysis center config
    
    * add icons and href
    
    * update Dockerfile
    
    * add to ignores
    
    * Run App Link works as expected
    
    ---------
    
    Co-authored-by: craigrbarnes <craig.barnes@intelligentlight.com>

--- packages/frontend/src/index.ts
+++ packages/frontend/src/index.ts
@@ -9,6 +9,7 @@ export * from './components/Protected';
 export * from './features/CohortBuilder';
 export * from './features/Query';
 export * from './features/Workspace';
+export * from './features/Analysis';
 export * from './utils/';
 
 import { getNavPageLayoutPropsFromConfig } from './lib/common/staticProps';
@@ -56,6 +57,12 @@ import { SubmissionPageGetServerSideProps } from './pages/Submission/data';
 import WorkspacesPage from './pages/Workspace/Workspaces';
 import { WorkspacesPageGetServerSideProps } from './pages/Workspace/data';
 
+import AnalysisPage from './pages/Analysis/Analysis';
+import { AnalysisPageGetServerSideProps } from './pages/Analysis';
+
+import AnalysisEditorPage from './pages/admin/analysis/Analysis';
+import { AnalysisEditorPageGetServerSideProps } from './pages/admin/analysis/data';
+
 import AiSearchPage from './pages/AiSearch/AiSearch';
 import { AISearchPageGetServerSideProps } from './pages/AiSearch/data';
 
@@ -102,6 +109,8 @@ export {
   AdminAuthZPageGetServerSideProps,
   WorkspacesPage,
   WorkspacesPageGetServerSideProps,
+  AnalysisPage,
+  AnalysisPageGetServerSideProps,
   Custom404Page,
   sessionToken,
   sessionLogout,
@@ -113,4 +122,6 @@ export {
   CrosswalkPageGetServerSideProps,
   SubmissionPage,
   SubmissionPageGetServerSideProps,
+  AnalysisEditorPage,
+  AnalysisEditorPageGetServerSideProps,
 };
