0533cc53de8884d90c4b11bb41abd881223c7c66
Craig Barnes <craig.barnes@intelligentlight.com>
2024-11-13 16:29:09 +0100

    GFF-194: add sower jobs support for data library (#267)
    * add sowerApi.ts
    
    * add sowerApi to index
    
    * add GEN3_SOWER_API to index

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -11,6 +11,7 @@ import {
   GEN3_WORKSPACE_STATUS_API,
   GEN3_SUBMISSION_API,
   GEN3_CROSSWALK_API,
+  GEN3_SOWER_API,
   Accessibility,
 } from './constants';
 import { type CoreState } from './reducers';
@@ -36,6 +37,7 @@ export * from './features/grip';
 export * from './features/aiSearch';
 export * from './features/workspace';
 export * from './features/submission';
+export * from './features/sower';
 
 export {
   type CoreState,
@@ -51,5 +53,6 @@ export {
   GEN3_WORKSPACE_STATUS_API,
   GEN3_SUBMISSION_API,
   GEN3_CROSSWALK_API,
+  GEN3_SOWER_API,
   Accessibility,
 };
