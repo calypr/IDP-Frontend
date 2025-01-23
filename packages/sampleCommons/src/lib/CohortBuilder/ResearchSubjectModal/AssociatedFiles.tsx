aeeb2d18b983b9df67eeab927d099484c80ff65c
matthewpeterkort <matthewpeterkort@gmail.com>
2025-01-22 16:32:00 -0800

    swap mantine table for matching table

--- packages/sampleCommons/src/lib/CohortBuilder/ResearchSubjectModal/AssociatedFiles.tsx
+++ packages/sampleCommons/src/lib/CohortBuilder/ResearchSubjectModal/AssociatedFiles.tsx
@@ -4,8 +4,9 @@ import { ErrorCard } from '@gen3/frontend';
 import { useGeneralGQLQuery, JSONObject } from '@gen3/core';
 import { isQueryResponse, extractData } from './tools';
 import React, { useMemo, useState } from 'react';
+import { MatchingTable } from '@gen3/frontend';
 
-export const useFilesQuery = (identifiers: string[]) => {
+export const useFilesQuery = (identifiers: string[], table: boolean) => {
   const { data, isLoading, isError } = useGeneralGQLQuery({
     query: `query ($filter: JSON) {
               file (filter: $filter, accessibility: all, first: 10000) {
@@ -29,16 +30,15 @@ export const useFilesQuery = (identifiers: string[]) => {
       },
     },
   });
-
-  const cachedData = useMemo(() => {
-    if (data) {
+  const cachedData = useMemo<JSONObject[]>(() => {
+    if (data && !table) {
       const extractedData = isQueryResponse(data)
-        ? extractData(data, 'file', '')
+        ? (extractData(data, 'file', '') as JSONObject[])
         : [];
       return extractedData;
     }
-    return [];
-  }, [data]);
+    return Array.isArray(data) ? (data as JSONObject[]) : [];
+  }, [data, table]);
 
   return { resData: cachedData, isLoading, isError };
 };
@@ -50,14 +50,14 @@ export const UniqueAssociatedValsForSpecimen = ({
   identifiers: string[];
   asoc_val: string;
 }) => {
-  const { resData, isLoading, isError } = useFilesQuery(identifiers);
+  const { resData, isLoading, isError } = useFilesQuery(identifiers, false);
   if (isError) {
     return <Text> Error occurred while fetching file metadata </Text>;
   }
   if (!isLoading) {
-    const ResourceList = [...new Set(resData.map((val) => val[asoc_val]))].join(
-      ', ',
-    );
+    const ResourceList = [
+      ...new Set(resData?.map((val) => val[asoc_val])),
+    ].join(', ');
     return ResourceList;
   }
 };
@@ -69,23 +69,17 @@ export const AssociatedFilesText = ({
   identifiers: string[];
   asoc_val: string;
 }) => {
-  const { resData, isLoading, isError } = useFilesQuery(identifiers);
+  const { resData, isLoading, isError } = useFilesQuery(identifiers, false);
   // Return the length, loading, and error status
   if (isError) {
     return <Text> Error occurred while fetching data </Text>;
   }
-  const vals = UniqueAssociatedValsForSpecimen({
-    identifiers: identifiers,
-    asoc_val: asoc_val,
-  });
 
   return (
     <div>
       <LoadingOverlay visible={isLoading} />
-      <Text>
-        {identifiers.length} Annotations, {resData.length} Files From
-      </Text>
-      <Text>{vals}</Text>
+      <Text>{identifiers.length} Annotations</Text>
+      <Text>{resData?.length} Files</Text>
     </div>
   );
 };
@@ -97,10 +91,16 @@ export const AssociatedAssaysTable = ({
   identifiers: string[];
   asoc_val: string;
 }) => {
-  const { resData, isLoading, isError } = useFilesQuery(identifiers);
+  const { resData, isLoading, isError } = useFilesQuery(identifiers, true);
+  const {
+    resData: resDataTwo,
+    isLoading: isLoadingTwo,
+    isError: isErrorTwo,
+  } = useFilesQuery(identifiers, false);
+
   const [showTable, setshowTable] = useState(false);
 
-  if (isError) {
+  if (isError || isErrorTwo) {
     return <ErrorCard message={'Error occurred while fetching data'} />;
   }
 
@@ -110,44 +110,59 @@ export const AssociatedAssaysTable = ({
     return left - right;
   });
 
+  const filteredResourcesTwo = resDataTwo.toSorted(
+    (a: JSONObject, b: JSONObject) => {
+      const left = a[asoc_val] as number;
+      const right = b[asoc_val] as number;
+      return left - right;
+    },
+  );
+
+  const asocFileConfig = {
+    title: {
+      title: 'File Name',
+      field: 'title',
+    },
+    experimental_strategy: {
+      title: 'Assay',
+      field: 'experimental_strategy',
+    },
+    specimen_indexed_collection_date_days: {
+      title: 'Indexd Days',
+      field: 'specimen_indexed_collection_date_days',
+    },
+    specimen_sample_family_id: {
+      title: 'Sample Family Id',
+      field: 'specimen_sample_family_id',
+    },
+  };
+
+  // Headers : File Name, Assay, Indexd Days, Sample Family Id
+  // Guppy: experimental_strategy, specimen_indexed_collection_date_days, specimen_sample_family_id
+  //
   return (
     <Stack>
-      <LoadingOverlay visible={isLoading} />
+      <LoadingOverlay visible={isLoading || isLoadingTwo} />
       <div className="pt-2">
         <Checkbox
           label="Toggle File / Assay"
           onChange={() => setshowTable(!showTable)}
         ></Checkbox>
       </div>
-      {resData.length > 0 && showTable ? (
+      {showTable ? (
         <div className="text-primary">
-          <Table>
-            <Table.Thead>
-              <Table.Tr>
-                <Table.Th>File Name</Table.Th>
-                <Table.Th>Assay</Table.Th>
-                <Table.Th>Indexd Days</Table.Th>
-                <Table.Th> Sample Family Id </Table.Th>
-              </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>
-              {filteredResources.map((element) => (
-                <Table.Tr
-                  key={`${element.experimental_strategy}-${element.title}`}
-                >
-                  <Table.Td>{element.title}</Table.Td>
-                  <Table.Td>{element.experimental_strategy}</Table.Td>
-                  <Table.Td>
-                    {element.specimen_indexed_collection_date_days}
-                  </Table.Td>
-                  <Table.Td>{element.specimen_sample_family_id}</Table.Td>
-                </Table.Tr>
-              ))}
-            </Table.Tbody>
-          </Table>
+          <div className="grid">
+            <MatchingTable
+              isLoading={isLoading}
+              columns={asocFileConfig}
+              index={'file'}
+              idField={'id'}
+              data={filteredResources}
+            />
+          </div>
         </div>
       ) : (
-        <AssayCheckboxChart data={filteredResources} />
+        <AssayCheckboxChart data={filteredResourcesTwo} />
       )}
     </Stack>
   );
