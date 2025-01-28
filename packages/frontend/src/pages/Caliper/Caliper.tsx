9e96e4bd90f02b5d0ef63270d1c73955a3ce3b3a
matthewpeterkort <matthewpeterkort@gmail.com>
2025-01-28 08:54:15 -0800

    cleaup Caliper landing page

--- packages/frontend/src/pages/Caliper/Caliper.tsx
+++ packages/frontend/src/pages/Caliper/Caliper.tsx
@@ -1,4 +1,12 @@
-import { BackgroundImage, Text, Card, Button, Image } from '@mantine/core';
+import {
+  BackgroundImage,
+  Card,
+  Anchor,
+  Text,
+  Button,
+  Divider,
+  Image,
+} from '@mantine/core';
 import { CaliperLandingPageProps } from './types';
 import { LandingPageProps } from '../../components/Content/LandingPageContent';
 import { NavPageLayout } from '../../features/Navigation';
@@ -25,69 +33,71 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
         key: 'gen3-landing-page',
       }}
     >
-      <div className="flex flex-col gap-24 w-full">
-        <div className="relative w-full h-[350px]">
-          <BackgroundImage
-            className="absolute inset-0 bg-cover bg-center"
-            src="./images/landing/dna-blue-banner-8x3.jpg"
-          >
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black to-black opacity-60 z-10"></div>
-          </BackgroundImage>
+      <div className="flex flex-col w-full">
+        <div className="flex flex-col w-full gap-24">
+          <div className="relative w-full h-[350px]">
+            <BackgroundImage
+              className="absolute inset-0 bg-cover bg-center"
+              src="./images/landing/dna-blue-banner-8x3.jpg"
+            >
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black to-black opacity-60 z-10"></div>
+            </BackgroundImage>
 
-          <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-20">
-            <div className="text-4xl font-bold pb-7">CALIPER</div>
-            <div className="text-xl font-semibold pb-10">
-              Integrated data system tracking OHSU Knight Cancer research
-              datasets
-            </div>
-            <LoginMenu classNames={defaultClassNames}>Login</LoginMenu>
-          </div>
-        </div>
-
-        <div className="relative w-full h-[350px]">
-          <BackgroundImage
-            className="absolute inset-0 bg-cover bg-center"
-            src="./images/landing/data_explore.png"
-          >
-            <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
-          </BackgroundImage>
-
-          <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
-            <div className="text-4xl font-bold pb-7">Find New Datasets</div>
-            <div className="text-xl font-semibold pb-10">
-              Find data to power your research project
+            <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-20">
+              <div className="text-4xl font-bold pb-7">CALIPER</div>
+              <div className="text-xl font-semibold pb-10">
+                Integrated data system tracking OHSU Knight Cancer research
+                datasets
+              </div>
+              <LoginMenu classNames={defaultClassNames}>Login</LoginMenu>
             </div>
           </div>
-        </div>
 
-        <div className="relative w-full h-[350px]">
-          <BackgroundImage
-            className="absolute inset-0 bg-cover bg-center"
-            src="./images/landing/image_viewer.png"
-          >
-            <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
-          </BackgroundImage>
+          <div className="relative w-full h-[350px]">
+            <BackgroundImage
+              className="absolute inset-0 bg-cover bg-center"
+              src="./images/landing/data_explore.png"
+            >
+              <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
+            </BackgroundImage>
 
-          <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
-            <div className="text-4xl font-bold pb-7">Visualize</div>
-            <div className="text-xl font-semibold pb-10">
-              Integrated image viewers
+            <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
+              <div className="text-4xl font-bold pb-7">Find New Datasets</div>
+              <div className="text-xl font-semibold pb-10">
+                Find data to power your research project
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="relative w-full h-[350px]">
-          <BackgroundImage
-            className="absolute inset-0 bg-cover bg-center"
-            src="./images/landing/data_analysis.png"
-          >
-            <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
-          </BackgroundImage>
+          <div className="relative w-full h-[350px]">
+            <BackgroundImage
+              className="absolute inset-0 bg-cover bg-center"
+              src="./images/landing/image_viewer.png"
+            >
+              <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
+            </BackgroundImage>
 
-          <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
-            <div className="text-4xl font-bold pb-7">Analyze Data</div>
-            <div className="text-xl font-semibold pb-10">
-              Tools for rapid data science
+            <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
+              <div className="text-4xl font-bold pb-7">Visualize</div>
+              <div className="text-xl font-semibold pb-10">
+                Integrated image viewers
+              </div>
+            </div>
+          </div>
+
+          <div className="relative w-full h-[350px]">
+            <BackgroundImage
+              className="absolute inset-0 bg-cover bg-center"
+              src="./images/landing/data_analysis.png"
+            >
+              <div className="absolute inset-0 bg-black opacity-60 z-10"></div>
+            </BackgroundImage>
+
+            <div className="relative z-20 flex flex-col justify-center items-center text-white p-4 pt-28">
+              <div className="text-4xl font-bold pb-7">Analyze Data</div>
+              <div className="text-xl font-semibold pb-10">
+                Tools for rapid data science
+              </div>
             </div>
           </div>
         </div>
@@ -95,13 +105,7 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
         <div className="bg-gray-100 md:px-10 px-4 py-12">
           <div className="max-w-5xl max-lg:max-w-3xl max-sm:max-w-sm mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-sm:gap-8">
-              <Card
-                padding={0}
-                shadow="sm"
-                radius="md"
-                withBorder
-                className="flex flex-col"
-              >
+              <div className="bg-white rounded">
                 <Image
                   src="./images/landing/smmart-circle-diagram.png"
                   alt="Precision Oncology"
@@ -114,13 +118,15 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
                   <div className="text-gray-500 text-sm">SMMART</div>
                   <Button
                     component="a"
-                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.ohsu.edu/knight-cancer-institute/smmart-clinical-trials-research"
                     className="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
                   >
                     Read More
                   </Button>
                 </div>
-              </Card>
+              </div>
 
               <div className="bg-white rounded">
                 <Image
@@ -135,7 +141,9 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
                   <div className="text-gray-500 text-sm">CEDAR</div>
                   <Button
                     component="a"
-                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.ohsu.edu/knight-cancer-institute/cedar"
                     className="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
                   >
                     Read More
@@ -156,7 +164,9 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
                   <div className="text-gray-500 text-sm">CBDS</div>
                   <Button
                     component="a"
-                    href="#"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.ohsu.edu/knight-cancer-institute/center-biomedical-data-science"
                     className="mt-4 inline-block px-4 py-2 rounded tracking-wider bg-orange-500 hover:bg-orange-600 text-white text-[13px]"
                   >
                     Read More
@@ -167,55 +177,69 @@ const CaliperPage = ({ headerProps, footerProps }: Props) => {
           </div>
         </div>
 
-        <div class="bg-gray-50 sm:px-6 px-4 py-12 font-sans">
-          <div class="max-w-7xl mx-auto">
-            <div class="text-left">
-              <h2 class="text-3xl font-extrabold text-gray-800 inline-block">
+        <div className="bg-gray-50 sm:px-6 px-4 py-12 font-sans">
+          <div className="max-w-7xl mx-auto">
+            <div className="text-left">
+              <Text className="text-3xl font-extrabold text-gray-800 inline-block">
                 Publications
-              </h2>
+              </Text>
             </div>
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-16 max-md:max-w-lg mx-auto">
-              <div class="cursor-pointer rounded overflow-hidden group">
-                <div>
-                  <span class="text-sm block text-gray-400 mb-2">
-                    December 2024
-                  </span>
-                  <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-16 max-md:max-w-lg mx-auto">
+              <div>
+                <Text className="text-sm block text-gray-400 mb-2">
+                  December 2024
+                </Text>
+                <div className="cursor-pointer rounded overflow-hidden group">
+                  <Anchor
+                    className="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all"
+                    href="https://genomebiology.biomedcentral.com/articles/10.1186/s13059-024-03431-3"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    underline="never"
+                  >
                     SyntheVAEiser: augmenting traditional machine learning
                     methods with VAE-based gene expression sample generation for
                     improved cancer subtype predictions.
-                  </h3>
-                  <div class="mt-4">
-                    <p class="text-gray-400 text-sm">
-                      Karlberg B, Kirchgaessner R, Lee J, Peterkort M, Beckman
-                      L, Goecks J, Ellrott K.
-                    </p>
-                    <p class="text-gray-400 text-sm">
-                      Genome Biol. 2024 Dec 18;25(1):309. doi:
-                      10.1186/s13059-024-03431-3.
-                    </p>
-                  </div>
+                  </Anchor>
                 </div>
-                <hr class="my-5 border-gray-300" />
+
+                <div className="mt-4">
+                  <Text className="text-gray-400 text-sm">
+                    Karlberg B, Kirchgaessner R, Lee J, Peterkort M, Beckman L,
+                    Goecks J, Ellrott K.
+                  </Text>
+                  <Text className="text-gray-400 text-sm">
+                    Genome Biol. 2024 Dec 18;25(1):309. doi:
+                    10.1186/s13059-024-03431-3.
+                  </Text>
+                </div>
+                <Divider className="my-5 border-gray-300" />
               </div>
-              <div class="cursor-pointer rounded overflow-hidden group">
-                <div>
-                  <span class="text-sm block text-gray-400 mb-2">
-                    Janurary 2023
-                  </span>
-                  <h3 class="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all">
+              <div>
+                <Text className="text-sm block text-gray-400 mb-2">
+                  Janurary 2023
+                </Text>
+                <div className="cursor-pointer rounded overflow-hidden group">
+                  <Anchor
+                    className="text-xl font-bold text-gray-800 group-hover:text-blue-500 transition-all"
+                    href="https://www.sciencedirect.com/science/article/pii/S2666979X21001063"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    underline="never"
+                  >
                     Inverting the model of genomics data sharing with the NHGRI
                     Genomic Data Science Analysis, Visualization, and
                     Informatics Lab-space
-                  </h3>
-                  <div class="mt-4">
-                    <p class="text-gray-400 text-sm">
-                      Cell Genom. 2022 Jan 12;2(1):100085. doi:
-                      10.1016/j.xgen.2021.100085. Epub 2022 Jan 13
-                    </p>
-                  </div>
+                  </Anchor>
                 </div>
-                <hr class="my-5 border-gray-300" />
+
+                <div className="mt-4">
+                  <Text className="text-gray-400 text-sm">
+                    Cell Genom. 2022 Jan 12;2(1):100085. doi:
+                    10.1016/j.xgen.2021.100085. Epub 2022 Jan 13
+                  </Text>
+                </div>
+                <Divider className="my-5 border-gray-300" />
               </div>
             </div>
           </div>
