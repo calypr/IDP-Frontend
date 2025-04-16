96f0c0f328d08f15418eb0c25b5e68de485bdbaf
quinnwai <quinnwai.wong@gmail.com>
2025-04-16 16:29:48 -0700

    big boi refactor to be softer on the eyes

---
+++ packages/sampleCommons/src/lib/CohortBuilder/SpecimenModal/MenuTreeView/MenuTreeView.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { ResourceDict } from '../../types';
+import { edgesToNestedTree } from './MenuTreeAlgo';
+import { readTemplate, replaceIdsWithLabels, sortJsonKeys } from './MenuTreeHelpers';
+
+import { StaticTreeDataProvider, Tree as ReactComplexTree, UncontrolledTreeEnvironment } from 'react-complex-tree';
+import 'react-complex-tree/lib/style-modern.css';
+
+const MenuTreeView = ({
+    specimenDicts,
+    edges,
+    specimenIdToLabel,
+    focusId,
+  }: {
+    specimenDicts: ResourceDict[];
+    edges: [string, string][];
+    specimenIdToLabel: Record<string, string>;
+    focusId: string;
+  }) => {
+    const nestedTree = edgesToNestedTree(edges);
+  
+    // format tree with identifiers
+    const labeledTree = replaceIdsWithLabels(specimenIdToLabel, nestedTree);
+    const keySortedTree = sortJsonKeys(labeledTree);
+    const treeWithRoot = {
+      root: keySortedTree
+    };
+  
+    const formattedTree = readTemplate(treeWithRoot);
+    const dataProvider = new StaticTreeDataProvider(formattedTree.items, (item, data) => ({ ...item, data }));
+  
+    // get parent ids to highlight the path to the focus node
+    const childEdgeMap = specimenDicts.reduce((acc, specimen: ResourceDict) => {
+      acc[specimen.id] = specimen.parent;
+      return acc;
+    }, {}) as Record<string, string>;
+  
+    const specimenParentIds = [focusId];
+    let specimen = focusId;
+    while(specimen in childEdgeMap && childEdgeMap[specimen]) {
+      specimen = childEdgeMap[specimen];
+      specimenParentIds.push(specimen);
+    }
+    const specimenParentLabels = specimenParentIds.map((id) => specimenIdToLabel[id]);
+    const focusLabel = specimenIdToLabel[focusId];
+  
+    return (
+        
+      <UncontrolledTreeEnvironment
+        dataProvider={dataProvider}
+        getItemTitle={item => item.data}
+        viewState={{}}
+        renderItemTitle={({title}) => (
+          specimenParentLabels.includes(title)
+            ? title == focusLabel 
+              ? (<strong>{title} *</strong>)
+              : (<strong>{title}</strong>)
+            : title
+          )
+        }
+        onFocusItem={() => {}}
+      >
+        <ReactComplexTree
+          treeId="specimen-tree"
+          rootItem="root"
+          treeLabel="Specimen Tree"
+        />
+      </UncontrolledTreeEnvironment>
+    );
+  };
+
+export default MenuTreeView;
--- packages/sampleCommons/src/lib/CohortBuilder/SpecimenModal/SpecimenTree.tsx
+++ packages/sampleCommons/src/lib/CohortBuilder/SpecimenModal/SpecimenTree.tsx
@@ -1,32 +1,17 @@
 import { useGeneralGQLQuery } from '@gen3/core';
-import { StaticTreeDataProvider, Tree as ReactComplexTree, UncontrolledTreeEnvironment } from 'react-complex-tree';
-import 'react-complex-tree/lib/style-modern.css';
+import React from 'react';
+
+import GraphTreeView from './GraphTreeView/GraphTreeView';
+import MenuTreeView from './MenuTreeView/MenuTreeView';
 import { extractData, isQueryResponse } from '../ResearchSubjectModal/tools';
 import { QueryContent, ResourceDict } from '../types';
-import { edgesToNestedTree } from './SpecimenTreeAlgo';
-import { readTemplate, replaceIdsWithLabels, sortJsonKeys } from './SpecimenTreeHelpers';
 
-import React from 'react';
-import '@xyflow/react/dist/style.css';
-import { stratify, tree } from 'd3-hierarchy';
-import {
-  Controls,
-  ReactFlow,
-  ReactFlowProvider,
-} from '@xyflow/react';
-import * as d3 from 'd3';
-import SimpleNode from './SimpleNode';
-
-interface TreeNode {
+export interface TreeNode {
   name: string;
   id: string;
-  data: Record<string,any>;
+  data: Record<string, any>;
   children?: TreeNode[];
 }
-
-const nodeTypes = {
-  simpleNode: SimpleNode
-};
 
 const SpecimenTree = ({
     projectId, // required in case the same sample family ids is used across projects
@@ -41,6 +26,7 @@ const SpecimenTree = ({
     sampleTypeField: string;
     graphView: boolean;
   }) => {
+
     // get all specimens in the same family
     const { data: specimenFamilyData, isLoading: familyIsLoading, isError: familyIsError } = useGeneralGQLQuery({
       query: `query ($filter: JSON) {
@@ -81,33 +67,6 @@ const SpecimenTree = ({
 
     // convert to edge list from parent to child
     const edges = specimenDicts.map((specimen: ResourceDict) => [specimen.parent, specimen.id]) as [string, string][];
-
-    const nestedTree = edgesToNestedTree(edges);
-
-    // format tree with identifiers
-    const labeledTree = replaceIdsWithLabels(specimenIdToLabel, nestedTree);
-    const keySortedTree = sortJsonKeys(labeledTree);
-    const treeWithRoot = {
-      root: keySortedTree
-    };
-
-    const formattedTree = readTemplate(treeWithRoot);
-    const dataProvider = new StaticTreeDataProvider(formattedTree.items, (item, data) => ({ ...item, data }));
-
-    // get parent ids to highlight for 
-    const focusId = specimenId;
-    const focusLabel = specimenIdToLabel[focusId];
-    const childEdgeMap = specimenDicts.reduce((acc, specimen: ResourceDict) => {
-      acc[specimen.id] = specimen.parent;
-      return acc;
-    }, {}) as Record<string, string>;
-    const specimenParentIds = [focusId];
-    let specimen = focusId;
-    while(specimen in childEdgeMap && childEdgeMap[specimen]) {
-      specimen = childEdgeMap[specimen];
-      specimenParentIds.push(specimen);
-    }
-    const specimenParentLabels = specimenParentIds.map((id) => specimenIdToLabel[id]);
     
     // handle load and error states
     if (familyIsLoading) {
@@ -116,115 +75,20 @@ const SpecimenTree = ({
     if (familyIsError) {
       return <div>Error loading Specimen Tree</div>;
     }
-
-    // show graph view
-    if (graphView && !familyIsLoading) {
-      // build tree using labels into d3-hierarchy form 
-      const d3Edges = edges.map((edge) => ({parentId: specimenIdToLabel[edge[0]], id: specimenIdToLabel[edge[1]]})) as Record<string,string>[];
-
-      // create root, providing a data.label for each node
-      const root = stratify()(d3Edges)
-        .sort(
-          (a, b) => b.height - a.height || d3.ascending(a.id, b.id)
-        ) as d3.HierarchyNode<TreeNode>;
-      
-      const d3Tree = tree<TreeNode>()(root);
-      
-
-      // get all ancestors of the node with the same id as the focusId
-      const focusNode = d3Tree.descendants().find((node) => node.data.id === focusLabel);
-      const focusAncestors: Set<string> = focusNode 
-        ? new Set(focusNode.ancestors().map((ancestor) => ancestor.id) as string[])
-        : new Set();
-
-      // create nodes
-      const reactFlowNodes = d3Tree.descendants().map((node) => {
-        const val = {
-          id: node.data.id,
-          type: 'simpleNode',
-          data: {
-            ...node.data,
-            label: node.id,
-            isAncestor: node.id && focusAncestors.has(node.id),
-          },
-          // font bolded if the node is an ancestor of the focus node
-          style: {
-            fontWeight: node.id && focusAncestors.has(node.id) ? 'bold' : 'normal',
-          },
-          position: {x: node.y * 1400, y: node.x * 1400 } // horizontal tree
-        };
-        return val;
-      });
-
-      // create edges with conditional styles
-      const ancestorEdgeStyle = {
-        stroke: 'black',
-        strokeWidth: 2
-      };
-
-      const reactFlowEdges = d3Edges.map((edge) => (
-        {
-          id: `${edge.parentId}-${edge.id}`,
-          data: {
-            betweenAncestors: focusAncestors.has(edge.parentId) && focusAncestors.has(edge.id),
-          },
-          // make the line thicker if the edge is between two ancestors of the focus node
-          style: focusAncestors.has(edge.parentId) && focusAncestors.has(edge.id)  
-            ? ancestorEdgeStyle
-            : {},
-          target: edge.parentId,
-          source: edge.id
-        }
-      ));
-
-      console.log('reactFlowNodes:', reactFlowNodes);
-      console.log('reactFlowEdges:', reactFlowEdges);
-      
-      return (
-        <div style={{
-          width: '100%',
-          height: 800
-        }}
-        >
-          <ReactFlowProvider>
-            <ReactFlow
-              nodes={reactFlowNodes}
-              edges={reactFlowEdges}
-              nodeTypes={nodeTypes}
-              zoomOnScroll={false}
-              preventScrolling={false}
-              fitView
-            >
-              <Controls showZoom={true} />
-            </ReactFlow>
-          </ReactFlowProvider>
-        </div>
-      );
-    }
     
-    // render tree using react complex tree
-    return (
-      <UncontrolledTreeEnvironment
-        dataProvider={dataProvider}
-        getItemTitle={item => item.data}
-        viewState={{}}
-        renderItemTitle={({title}) => (
-          specimenParentLabels.includes(title)
-            ? title == focusLabel 
-              ? (<strong>{title} *</strong>)
-              : (<strong>{title}</strong>)
-            : title
-          )
-        }
-        onFocusItem={() => {}}
-      >
-        <ReactComplexTree
-          treeId="specimen-tree"
-          rootItem="root"
-          treeLabel="Specimen Tree"
-        />
-      </UncontrolledTreeEnvironment>
-    );
+    // render tree depending on the view
+    return graphView ?
+    <GraphTreeView
+      edges={edges}
+      specimenIdToLabel={specimenIdToLabel}
+      focusId={specimenId}
+    /> :
+    <MenuTreeView
+      specimenDicts={specimenDicts}
+      edges={edges}
+      specimenIdToLabel={specimenIdToLabel}
+      focusId={specimenId}
+    />;
 }; 
 
 export default SpecimenTree;
