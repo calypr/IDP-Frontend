105d89e91d53ae1e6c4fd8e52a64c86e51d4e8d9
matthewpeterkort <matthewpeterkort@gmail.com>
2026-05-18 15:55:23 -0700

    fix lint

--- packages/sampleCommons/src/pages/organization/[org]/index.tsx
+++ packages/sampleCommons/src/pages/organization/[org]/index.tsx
@@ -1,4 +1,4 @@
-import type { ComponentProps } from 'react';
+import React, { type ComponentProps } from 'react';
 import { useRouter } from 'next/router';
 import {
   OrganizationExplorerPageGetServerSideProps as getServerSideProps,
